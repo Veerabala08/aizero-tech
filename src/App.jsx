416cb10bdae006b92aa9182f141924651041178e
veerabala <imveerabala@gmail.com>
2025-04-08 23:59:37 +0530

    config

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/aizero-tech">
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
